f74dcc1e6d011fce1cb62cfbf065400d5b6037a8
vvauban <vvauban@gmail.com>
2026-03-12 05:27:46 +0100

    docs: clarify JPA mapping and repository/controller intent

--- src/main/java/com/vv/personaddresszipcode/model/Address.java
+++ src/main/java/com/vv/personaddresszipcode/model/Address.java
@@ -7,61 +7,81 @@ import jakarta.persistence.Id;
 
 import java.util.Objects;
 
+// Marks this class as a JPA entity.
+// It will be mapped to a database table.
 @Entity
 public class Address {
 
+    // Primary key of the entity.
     @Id
+
+    // Tells JPA to generate the ID automatically using the database identity strategy.
     @GeneratedValue( strategy = GenerationType.IDENTITY )
     private Long id;
 
+    // Street name and number of the address.
     private String street;
+
+    // City of the address.
     private String city;
+
+    // ZIP code of the address.
     private String zipCode;
 
-    // JPA needs a no-args constructor
+    // No-argument constructor required by JPA.
     public Address() {
     }
 
+    // Convenient constructor to create an Address with its main fields.
     public Address( String street, String city, String zipCode ) {
         this.street = street;
         this.city = city;
         this.zipCode = zipCode;
     }
 
-    // Getters / Setters
+    // Returns the database ID of this address.
     public Long getId() {
         return id;
     }
 
-    public void setId( Long id ) { // usually not used, but kept for completeness
+    // Sets the ID manually.
+    // In practice, this is usually managed by JPA.
+    public void setId( Long id ) {
         this.id = id;
     }
 
+    // Returns the street value.
     public String getStreet() {
         return street;
     }
 
+    // Updates the street value.
     public void setStreet( String street ) {
         this.street = street;
     }
 
+    // Returns the city value.
     public String getCity() {
         return city;
     }
 
+    // Updates the city value.
     public void setCity( String city ) {
         this.city = city;
     }
 
+    // Returns the ZIP code value.
     public String getZipCode() {
         return zipCode;
     }
 
+    // Updates the ZIP code value.
     public void setZipCode( String zipCode ) {
         this.zipCode = zipCode;
     }
 
-    // equals / hashCode (typical JPA-safe approach: compare by id when present)
+    // Compares two Address objects.
+    // For JPA entities, equality is based on the ID when it is available.
     @Override
     public boolean equals( Object o ) {
         if ( this == o ) {
@@ -73,11 +93,14 @@ public class Address {
         return id != null && Objects.equals( id, other.id );
     }
 
+    // Returns a hash code consistent with the equals implementation.
+    // Using the class hash code is a common JPA-safe pattern.
     @Override
     public int hashCode() {
         return getClass().hashCode();
     }
 
+    // Returns a readable string representation of the object.
     @Override
     public String toString() {
         return "Address{" +
--- src/main/java/com/vv/personaddresszipcode/model/Person.java
+++ src/main/java/com/vv/personaddresszipcode/model/Person.java
@@ -4,55 +4,78 @@ import jakarta.persistence.*;
 
 import java.util.Objects;
 
+// Marks this class as a JPA entity.
+// It will be mapped to a database table.
 @Entity
 public class Person {
 
+    // Primary key of the entity.
     @Id
+
+    // Tells JPA to generate the ID automatically using the database identity strategy.
     @GeneratedValue( strategy = GenerationType.IDENTITY )
     private Long id;
 
+    // Name of the person.
     private String name;
 
-    @OneToOne( cascade = CascadeType.ALL,
-               orphanRemoval = true )
-    @JoinColumn( name = "address_id" ) // optional but recommended to control FK column name
+    // Defines a one-to-one relationship between Person and Address.
+    // cascade = ALL means operations on Person (persist, remove, etc.)
+    // are also applied to the linked Address.
+    // orphanRemoval = true means that if the Address is no longer linked
+    // to this Person, it can be removed from the database.
+    @OneToOne(
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+
+    // Specifies the foreign key column name in the Person table.
+    @JoinColumn( name = "address_id" )
     private Address address;
 
-    // JPA needs a no-args constructor
+    // No-argument constructor required by JPA.
     public Person() {
     }
 
+    // Convenient constructor to create a Person with a name and an address.
     public Person( String name, Address address ) {
         this.name = name;
         this.address = address;
     }
 
-    // Getters / Setters
+    // Returns the database ID of this person.
     public Long getId() {
         return id;
     }
 
-    public void setId( Long id ) { // usually not used, but kept for completeness
+    // Sets the ID manually.
+    // In practice, this is usually managed by JPA.
+    public void setId( Long id ) {
         this.id = id;
     }
 
+    // Returns the person's name.
     public String getName() {
         return name;
     }
 
+    // Updates the person's name.
     public void setName( String name ) {
         this.name = name;
     }
 
+    // Returns the associated address.
     public Address getAddress() {
         return address;
     }
 
+    // Updates the associated address.
     public void setAddress( Address address ) {
         this.address = address;
     }
 
-    // equals / hashCode (JPA-safe: compare by id when present)
+    // Compares two Person objects.
+    // For JPA entities, equality is based on the ID when it is available.
     @Override
     public boolean equals( Object o ) {
         if ( this == o ) {
@@ -64,11 +87,14 @@ public class Person {
         return id != null && Objects.equals( id, other.id );
     }
 
+    // Returns a hash code consistent with the equals implementation.
+    // Using the class hash code is a common JPA-safe pattern.
     @Override
     public int hashCode() {
         return getClass().hashCode();
     }
 
+    // Returns a readable string representation of the object.
     @Override
     public String toString() {
         return "Person{" +
